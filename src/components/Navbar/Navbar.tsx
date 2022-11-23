ddee8229ffd588c2e388bc194fe0cbd4c9a711b5
sqhoward <stephenqhoward@gmail.com>
2022-11-23 17:49:22 -0500

    Added styling - settling on a color pallette

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { type FC } from "react";
 import Link from 'next/link';
 // import heroStyles from "./Hero.module.css";
 
@@ -14,16 +14,16 @@ type NavbarProps = {
 };
 
 const Navbar: FC<NavbarProps> = ({ logo, subTitle, links }) => (
-  <div className="container color-white flex flex-row max-h-10 scroll-smooth">
-    <div className="justify-self-start flex flex-row flex-grow">
+  <div className="container color-white flex flex-row max-h-10 bg-deep-blue items-center">
+    <div className="justify-self-start flex flex-row flex-grow h-12 items-center">
       <h2 className="pr-2">{ logo }</h2>
       <h3>{ subTitle }</h3>
     </div>
-    <div className="justify-self-end">
-      <ul className="flex flex-row">
+    <div className="justify-self-end items-center">
+      <ul className="flex flex-row items-center h-full">
         {links.map((link) => {
           return (
-            <li key={ link.displayText } className="px-5"><Link href={ link.anchor }>{ link.displayText }</Link></li>
+            <Link key={ link.displayText } href={ link.anchor } className="px-5 self-center h-full hover:bg-light-blue rounded-full"><li>{ link.displayText }</li></Link>
           )
         })}
       </ul>
