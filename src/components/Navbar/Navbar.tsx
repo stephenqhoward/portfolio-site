2e127a2ba95ae2a47c59fae037d37eef184ca147
sqhoward <stephenqhoward@gmail.com>
2022-11-24 11:06:11 -0500

    Styled footer - fixed responsive bug

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -15,15 +15,15 @@ type NavbarProps = {
 
 const Navbar: FC<NavbarProps> = ({ logo, subTitle, links }) => (
   <div className="container color-white flex flex-row max-h-10 bg-deep-blue items-center">
-    <div className="justify-self-start flex flex-row flex-grow h-12 items-center">
+    <div className="justify-self-start flex flex-row flex-grow flex-wrap h-12 items-center">
       <h2 className="pr-2">{ logo }</h2>
-      <h3>{ subTitle }</h3>
+      <h3 className="hidden md:flex">{ subTitle }</h3>
     </div>
     <div className="justify-self-end items-center">
       <ul className="flex flex-row items-center h-full">
         {links.map((link) => {
           return (
-            <Link key={ link.displayText } href={ link.anchor } className="px-5 self-center h-full hover:bg-light-blue rounded-full"><li>{ link.displayText }</li></Link>
+            <Link key={ link.displayText } href={ link.anchor } className="px-2 lg:px-6 self-center h-full hover:bg-light-blue rounded-full"><li>{ link.displayText }</li></Link>
           )
         })}
       </ul>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
 import { type NextPage } from "next";
 import Head from "next/head";
+import Image from 'next/image'
 import { Navbar } from "../components/Navbar";
+import GitHubLogo from '../../public/GitHub_Logo_White.png';
+import LinkedInLogo from '../../public/In-White-26.png';
 
 const Home: NextPage = () => {
   return (
@@ -11,27 +14,32 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex flex-col min-h-screen items-center justify-center bg-gradient-to-b from-[#15222c] to-[#022b6d]">
+        
         <Navbar logo="SQH" subTitle="Portfolio Site" links={[{displayText: 'About', anchor: '#about'}, {displayText: 'Portfolio', anchor: '#portfolio'}, {displayText: 'Social', anchor: '#social'}, {displayText: 'Resume', anchor: '#resume'} ]}></Navbar>
+
         <div className="container flex flex-col items-center justify-center">
-          <section className="min-h-screen min-w-full bg-light-blue">
-            <h1>Welcome to Stephen Q Howard&apos;s portfolio site.</h1>
-            <p>Please excuse the saw dust as I spin this up!</p>
+          
+          <section className="min-h-screen min-w-full bg-cream color-light-blue">
+            <h1 className="">Welcome to Stephen Q Howard&apos;s portfolio site.</h1>
+            <p className="">Please excuse the saw dust as I spin this up!</p>
           </section>
-          <section className="container min-h-[40vh] flex flex-row bg-blue" id="about">
-            <div className="pr-36">
+          
+          <section className="container min-h-[40vh] flex flex-row flex-wrap bg-blue" id="about">
+            <div className="mr-36 px-10">
               <h2>About Me</h2>
-              <ul>
-                <li>I am a recent graduate of a 28-week coding boot camp, while working full time. I am no stranger to a demanding work environment, getting my start out of college in a Big Four public accounting firm.</li>
-                <li>I have a history of researching and implementing technical solutions to business problems.</li>
-                <li>You will find I am self-motivated, highly adaptable, and one who pays close attention to the details.</li>
-                <li>I am looking for experience that will provide awareness for what I don&apos;t know, an understanding of the necessary structure for high performing software development teams, and the core technical competency to prototype my startup ideas.</li>
+              <ul className="">
+                <li className="pt-3">I am a recent graduate of a 28-week coding boot camp, while working full time. I am no stranger to a demanding work environment, getting my start out of college in a Big Four public accounting firm.</li>
+                <li className="pt-3">I have a history of researching and implementing technical solutions to business problems.</li>
+                <li className="pt-3">You will find I am self-motivated, highly adaptable, and one who pays close attention to the details.</li>
+                <li className="pt-3">I am looking for experience that will provide awareness for what I don&apos;t know, an understanding of the necessary structure for high performing software development teams, and the core technical competency to prototype my startup ideas.</li>
               </ul>
             </div>
-            <div>
+            <div className="px-10">
               <h2>Skills</h2>
               <p>JavaScript - Python - ReactJs - Django - PostgreSQL - Typescript - Tailwind - HTML - CSS - Git - GitHub - Slack</p>
             </div>
           </section>
+
           <section className="container min-h-[80vh] bg-cream" id="portfolio">
             <h2 className="">Portfolio</h2>
             <div>
@@ -41,14 +49,28 @@ const Home: NextPage = () => {
               <h3>Discing Up</h3>
             </div>
           </section>
-          <section className="container min-h-[40vh] bg-deep-blue" id="social">
-            <h2>Social</h2>
-            <ul>
-              <li>LinkedIn: <a href='https://www.linkedin.com/in/stephenqhoward/'>StephenQHoward</a></li>
-              <li>GitHub: <a href='https://github.com/stephenqhoward'>StephenQHoward</a></li>
-              <li>Resume: </li>
+
+          <section className="container min-h-[20vh] bg-deep-blue" id="social">
+            <ul className="flex flex-row justify-center items-center min-h-[50px] my-8">
+              <a href='https://www.linkedin.com/in/stephenqhoward/' className="mr-12 flex items-center bg-light-blue rounded-full">
+                <li className="flex h-[90px] w-[90px] place-self-center">
+                  <Image alt="LinkedIn" src={ LinkedInLogo } width={45} className="m-auto self-center"></Image>
+                </li>
+              </a>
+              <a href='https://github.com/stephenqhoward' className="bg-light-blue mr-12 rounded-full">
+                <li className="flex text-center justify-items-center h-[90px] w-[90px]">
+                  <Image alt="GitHub" src={ GitHubLogo } width={80} className="mx-auto self-center"></Image>
+                </li>
+              </a>
+              <a href='#resume' className="bg-light-blue rounded-full">
+                <li className="flex h-[90px] w-[90px]">
+                  <p className="self-center mx-auto font-bold">Resume
+                  </p>
+                </li>
+              </a>
             </ul>
           </section>
+
         </div>
       </main>
     </>
