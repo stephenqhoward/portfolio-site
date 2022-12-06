530026db884a200a0e49ae97260991dc6c9791a3
sqhoward <stephenqhoward@gmail.com>
2022-12-06 16:08:43 -0500

    Fixed children props issue

--- src/components/EducationItem/EducationItem.tsx
+++ src/components/EducationItem/EducationItem.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import { type imgs } from '../PortfolioItem/PortfolioItem';
 
 export type EducationItemProps = {
+  children: React.ReactNode;
   institution: string;
   description: string;
   technologies?: Array<imgs>;
