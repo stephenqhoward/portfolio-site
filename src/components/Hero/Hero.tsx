0470a0907cd477ef6fba9742a0bf4c2471d92c8a
sqhoward <stephenqhoward@gmail.com>
2022-12-12 13:04:08 -0500

    Combined h1 h2 for SEO - Adjusted colors for Accessibility

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -5,12 +5,10 @@ type HeroProps = {
 
 const Hero = ({ title, subtitle }: HeroProps) => (
   <div className="my-14 text-center">
-    <h1 className="font-bold text-4xl text-deep-blue">{ title }</h1>
-      <h2 className="text-2xl text-center">{ subtitle }</h2>
-      <br></br>
-      <p className="text-red">Please excuse the saw dust as I spin this up!</p>
-      <p className="text-deep-blue">Site Version 0.9</p>
-      <p className="font-thin text-deep-blue">Last Updated: 12/6/2022</p>
+    <h1 className="font-bold text-4xl text-deep-blue">{ title } 
+      <br />
+      <span className="text-2xl text-center text-blue">{ subtitle }</span>
+    </h1>
   </div>
 );
 
