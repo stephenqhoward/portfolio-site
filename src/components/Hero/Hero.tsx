1a7d51b35b0bb05edb6157a25509104ba5bd5e0e
sqhoward <stephenqhoward@gmail.com>
2023-02-27 13:00:04 -0500

    adj vh

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -4,7 +4,7 @@ type HeroProps = {
 };
 
 const Hero = ({ title, subtitle }: HeroProps) => (
-  <div className="my-14 text-center">
+  <div className="my-14 min-h-[60vh] text-center">
     <h1 className="text-4xl font-bold text-deep-blue">
       {title}
       <br />
