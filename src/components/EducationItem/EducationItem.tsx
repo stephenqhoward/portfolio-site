6837ea5341d74d0060f96bdd1d9b84e83e1b3e6c
sqhoward <stephenqhoward@gmail.com>
2023-03-21 13:04:03 -0400

    Adjust padding and font sizes

--- src/components/EducationItem/EducationItem.tsx
+++ src/components/EducationItem/EducationItem.tsx
@@ -25,15 +25,15 @@ const EducationItem = ({
   integratedApis,
 }: EducationItemProps) => {
   return (
-    <div className="flex w-full flex-col flex-wrap space-y-6 self-center rounded-lg border-light-blue bg-slate-800 pb-6 text-lg text-cream lg:w-[60%] lg:border-2 lg:p-4 lg:pl-0">
-      <div className="flex w-full flex-col space-y-2 px-2 lg:px-4">
+    <div className="flex w-full flex-col flex-wrap space-y-6 self-center rounded-lg border-light-blue bg-slate-800 p-4 pb-6 text-lg text-cream lg:w-[60%] lg:border-2 lg:pl-0">
+      <div className="flex w-full flex-col space-y-2 px-1 lg:px-4">
         <a href={url} target="_blank" rel="noreferrer">
-          <h3 className="mb-4 text-xl font-bold text-cream md:text-2xl">
+          <h3 className="mb-4 text-2xl font-bold text-cream md:text-2xl">
             {institution}
           </h3>
         </a>
-        <p>{description}</p>
-        {honors && <p>Honors: {honors}</p>}
+        <p className="text-sm md:text-base">{description}</p>
+        {honors && <p className="text-sm md:text-base">Honors: {honors}</p>}
         {technologies && (
           <>
             <p className="text-center underline">Tech Stack</p>
@@ -58,20 +58,21 @@ const EducationItem = ({
       </div>
       {projectTitle && (
         <div className="w-full px-2 lg:px-4">
-          <h4 className="text-xl font-bold italic text-cream">
+          <h4 className="text-lg font-bold italic text-cream">
             {projectTitle}
           </h4>
-          <ul>
+          <ul className="mb-6">
             {projectDescription?.map((bullet, index) => {
               return (
-                <li key={index} className="mx-auto mb-4 lg:mr-4">
-                  <p className="text-justify lg:text-left">{bullet}</p>
+                <li key={index} className="mx-auto mb-2 lg:mr-4">
+                  <p className="text-justify text-base lg:text-left">
+                    {bullet}
+                  </p>
                 </li>
               );
             })}
           </ul>
           <p className="pb-4 text-center underline">External APIs Integrated</p>
-          {/* lg:flex-nowrap */}
           <ul
             className={`mx-auto flex w-full flex-row items-center md:w-[70%] ${styles["technologies-item-buttons"]} spae-y-4 md:space-y-0 lg:flex-nowrap`}
           >
