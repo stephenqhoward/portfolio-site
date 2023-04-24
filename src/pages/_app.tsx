f033b04768c88037b885165e7ae1c900bde60d7b
sqhoward <stephenqhoward@gmail.com>
2023-04-24 14:26:42 -0400

    Remove semi-colon artifact

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,7 @@ import "../styles/globals.css";
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
       <Analytics />
     </>
   );
